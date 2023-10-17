efe04a6c7d55d3785726128359f0056f6a776f4a
kivetat-tatevik <kivetat.85@yandex.ru>
2023-10-17 22:15:55 +0200

    Initial Commit

---
+++ ColorizedSwiftUI/ColorizedSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedSwiftUIApp.swift
+//  ColorizedSwiftUI
+//
+//  Created by Tatevik Khunoyan on 17.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
